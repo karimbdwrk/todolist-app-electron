5f5539b7be56fea30220c84be8d3e2af2466a70c
Karim Badwork <karim@badwork.fr>
2023-11-02 17:17:00 +0100

    user sign in private route zand signout ok

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import { redirect } from "react-router-dom";
+import React, { useEffect } from "react";
+import { redirect, useNavigate } from "react-router-dom";
 import { auth } from "../services/firebase.config";
+import { signOut } from "firebase/auth";
 import { useAuth } from "../AuthContext";
 
 const DashboardPage = () => {
 	const { user } = useAuth();
+	const navigate = useNavigate();
+
+	const handleLogout = () => {
+		signOut(auth)
+			.then(() => {
+				// Sign-out successful.
+				navigate("/");
+				console.log("Signed out successfully");
+			})
+			.catch((error) => {
+				// An error happened.
+			});
+	};
+
+	useEffect(() => {
+		if (!user) {
+			navigate("/signin");
+		}
+	}, []);
+
 	return (
 		<>
 			<h1>Dashboard Page</h1>
-			<p>User: {user.email}</p>
+			<p>User: {user?.email}</p>
+			<button onClick={handleLogout}>Logout</button>
 		</>
 	);
 };
